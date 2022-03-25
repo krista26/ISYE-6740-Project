5b87ca4523eed2ba361529916d5c78da0f904d1e
krista26 <97413659+krista26@users.noreply.github.com>
2022-03-24 21:30:23 -0700

    Update project testing.py

--- project testing.py
+++ project testing.py
@@ -56,20 +56,57 @@ print(testing.shape)
 testinghealthy=healthylist[:500]
 
 
-
 #%%
 from sklearn.model_selection import train_test_split
 tumor=np.ones(len(tumorlist))
 notumor=np.zeros(len(healthylist))
 
 together=np.concatenate((tumorlist, healthylist))
-togethery=np.concatenate((tumor,notumor))
+togethery=np.concatenate((tumor,notumor)).reshape(-1,1)
+
+print(togethery.shape)
+
+data=np.append(together, togethery, axis=1)
+print(data.shape)
 
 #Train test split
-train, test= train_test_split(together, test_size=0.2)
-print(train.shape)
+train, test= train_test_split(data, test_size=0.2)
+xtrain=train[:,:4096]
+ytrain=train[:,4096]
+xtest=test[:,:4096]
+ytest=test[:,4096]
 
-#HELLO THIS IS SEAN
+# %% Logistic regression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+
+logreg=LogisticRegression(max_iter=500, solver='newton-cg').fit(xtrain,ytrain)
+print('Logistic Regression classification Report')
+preds=logreg.predict(xtest)
+print(confusion_matrix(ytest, preds))
+print(classification_report(ytest,preds))
+
+
+# %% Plotting 4 random tumors
+
+f, axarr = plt.subplots(1,4)
+rands=np.random.randint(0,len(tumorlist),4)
+axarr[0].imshow(tumorlist[rands[0]].reshape(64,64), cmap='gray')
+axarr[1].imshow(tumorlist[rands[1]].reshape(64,64),cmap='gray')
+axarr[2].imshow(tumorlist[rands[2]].reshape(64,64),cmap='gray')
+axarr[3].imshow(tumorlist[rands[3]].reshape(64,64),cmap='gray')
+plt.suptitle("4 Random Brains with Tumors", y=.7)
+plt.subplots_adjust(wspace=0.4)
+
+# %% Plotting 4 random healthy brains
+f, axarr = plt.subplots(1,4)
+rands=np.random.randint(0,len(healthylist),4)
+axarr[0].imshow(healthylist[rands[0]].reshape(64,64), cmap='gray')
+axarr[1].imshow(healthylist[rands[1]].reshape(64,64),cmap='gray')
+axarr[2].imshow(healthylist[rands[2]].reshape(64,64),cmap='gray')
+axarr[3].imshow(healthylist[rands[3]].reshape(64,64),cmap='gray')
+plt.suptitle("4 Random Brains without Tumors", y=.7)
+plt.subplots_adjust(wspace=0.4)
 
 
 
