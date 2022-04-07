f8327e765d34e993799d17f178c262646db0a91b
skim489 <62309934+skim489@users.noreply.github.com>
2022-04-07 19:52:48 -0400

    Adding authors and main function

--- project testing.py
+++ project testing.py
@@ -2,7 +2,11 @@
 """
 Created on Mon Mar  7 17:49:34 2022
 
-@author: krist
+@authors: 
+1.	Krista Radecke (GT ID: 903736778)
+2.	Sandra Kim (GT ID: 902745258)
+3.	Sean Lee (GT ID: 903648883)
+
 """
 
 import scipy.io
@@ -73,86 +77,89 @@ def whiteborders(img):
         cop[:,-1]=0
     return cop
 
-# %% Downloading both data sets and storing in arrays
 
-tumor1, healthy1=downloadimages(set1paths, valid_image)
-tumor2, healthy2=downloadimages(set2paths, valid_image)
+def main():
+    # %% Downloading both data sets and storing in arrays
 
-                
-           
-# %% testing
+    tumor1, healthy1=downloadimages(set1paths, valid_image)
+    tumor2, healthy2=downloadimages(set2paths, valid_image)
 
-print(len(tumor1))
-print(len(healthy1))
-print(len(tumor2))
-print(len(healthy2))
+                    
+            
+    # %% testing
 
-
-
-
-#%% Concatenating data sets, shuffling data, and creating test/train splits
-from sklearn.model_selection import train_test_split
-tumor=np.ones(len(tumor1))
-notumor=np.zeros(len(healthy1))
-
-together=np.concatenate((tumor1, healthy1))
-togethery=np.concatenate((tumor,notumor)).reshape(-1,1)
-
-print(togethery.shape)
-print(together.shape)
-
-data=np.append(together, togethery, axis=1)
-print(data.shape)
-
-#Shuffle data
-np.random.seed(2)
-shuff=np.random.permutation(len(data))
-data=data[shuff,:]
-
-#Train test split
-train, test= train_test_split(data, test_size=0.2)
-xtrain=train[:,:4096]
-ytrain=train[:,4096]
-xtest=test[:,:4096]
-ytest=test[:,4096]
-
-# %% Logistic regression
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-
-logreg=LogisticRegression(max_iter=500, solver='newton-cg').fit(xtrain,ytrain)
-print('Logistic Regression classification Report')
-preds=logreg.predict(xtest)
-print(confusion_matrix(ytest, preds))
-print(classification_report(ytest,preds))
+    print(len(tumor1))
+    print(len(healthy1))
+    print(len(tumor2))
+    print(len(healthy2))
 
 
-# %% Plotting 4 random tumors
-#Comment/uncomment to switch data set
-dataset=tumor1
-#dataset=tumor2
-
-f, axarr = plt.subplots(1,4)
-rands=np.random.randint(0,len(dataset),4)
-axarr[0].imshow(dataset[rands[0]].reshape(64,64), cmap='gray')
-axarr[1].imshow(dataset[rands[1]].reshape(64,64),cmap='gray')
-axarr[2].imshow(dataset[rands[2]].reshape(64,64),cmap='gray')
-axarr[3].imshow(dataset[rands[3]].reshape(64,64),cmap='gray')
-plt.suptitle("4 Random Brains with Tumors", y=.7)
-plt.subplots_adjust(wspace=0.4)
-
-# %% Plotting 4 random healthy brains
-#Comment/uncomment to switch data set
-#healthyset=healthy1
-healthyset=healthy2
-f, axarr = plt.subplots(1,4)
-rands=np.random.randint(0,len(healthyset),4)
-axarr[0].imshow(healthyset[rands[0]].reshape(64,64), cmap='gray')
-axarr[1].imshow(healthyset[rands[1]].reshape(64,64),cmap='gray')
-axarr[2].imshow(healthyset[rands[2]].reshape(64,64),cmap='gray')
-axarr[3].imshow(healthyset[rands[3]].reshape(64,64),cmap='gray')
-plt.suptitle("4 Random Brains without Tumors", y=.7)
-plt.subplots_adjust(wspace=0.4)
 
 
+    #%% Concatenating data sets, shuffling data, and creating test/train splits
+    from sklearn.model_selection import train_test_split
+    tumor=np.ones(len(tumor1))
+    notumor=np.zeros(len(healthy1))
+
+    together=np.concatenate((tumor1, healthy1))
+    togethery=np.concatenate((tumor,notumor)).reshape(-1,1)
+
+    print(togethery.shape)
+    print(together.shape)
+
+    data=np.append(together, togethery, axis=1)
+    print(data.shape)
+
+    #Shuffle data
+    np.random.seed(2)
+    shuff=np.random.permutation(len(data))
+    data=data[shuff,:]
+
+    #Train test split
+    train, test= train_test_split(data, test_size=0.2)
+    xtrain=train[:,:4096]
+    ytrain=train[:,4096]
+    xtest=test[:,:4096]
+    ytest=test[:,4096]
+
+    # %% Logistic regression
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.metrics import classification_report, confusion_matrix
+
+    logreg=LogisticRegression(max_iter=500, solver='newton-cg').fit(xtrain,ytrain)
+    print('Logistic Regression classification Report')
+    preds=logreg.predict(xtest)
+    print(confusion_matrix(ytest, preds))
+    print(classification_report(ytest,preds))
+
+
+    # %% Plotting 4 random tumors
+    #Comment/uncomment to switch data set
+    dataset=tumor1
+    #dataset=tumor2
+
+    f, axarr = plt.subplots(1,4)
+    rands=np.random.randint(0,len(dataset),4)
+    axarr[0].imshow(dataset[rands[0]].reshape(64,64), cmap='gray')
+    axarr[1].imshow(dataset[rands[1]].reshape(64,64),cmap='gray')
+    axarr[2].imshow(dataset[rands[2]].reshape(64,64),cmap='gray')
+    axarr[3].imshow(dataset[rands[3]].reshape(64,64),cmap='gray')
+    plt.suptitle("4 Random Brains with Tumors", y=.7)
+    plt.subplots_adjust(wspace=0.4)
+
+    # %% Plotting 4 random healthy brains
+    #Comment/uncomment to switch data set
+    #healthyset=healthy1
+    healthyset=healthy2
+    f, axarr = plt.subplots(1,4)
+    rands=np.random.randint(0,len(healthyset),4)
+    axarr[0].imshow(healthyset[rands[0]].reshape(64,64), cmap='gray')
+    axarr[1].imshow(healthyset[rands[1]].reshape(64,64),cmap='gray')
+    axarr[2].imshow(healthyset[rands[2]].reshape(64,64),cmap='gray')
+    axarr[3].imshow(healthyset[rands[3]].reshape(64,64),cmap='gray')
+    plt.suptitle("4 Random Brains without Tumors", y=.7)
+    plt.subplots_adjust(wspace=0.4)
+
+if __name__ == "__main__":
+    main()
 
